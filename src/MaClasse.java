7219348dd27f680fb56de9d49faf2fe8a6cb2e36
bastien <bastiencaumont@orange.fr>
2022-01-02 14:39:52 +0100

    Initial commit

---
+++ src/MaClasse.java
@@ -0,0 +1,23 @@
+public class MaClasse {
+    public static void main(String[] args){
+        //Déclarer la variable int Ongoingallowance
+        int ongoingAllowance=500;
+        //Déclarer la variable int Savings
+        int savings=3000;
+
+        //Déclarer une variable nommée int bonusALllowance avec la valeur 500
+        int bonusAllowance=500;
+        System.out.println("Hello "+ongoingAllowance);
+        int numberOfDaysToSave=(5000-ongoingAllowance)/500;
+        String maChaine="Je parle";
+        savings=20;
+        System.out.println(savings);
+        int i=0;
+        for(i=0;i<1;i++){
+            System.out.print("Hello");
+            System.out.print("Bonjour");
+        }
+
+
+    }
+}
